2c33e60a51f56ec80cd0a54c5ae5bc3488d921a1
nahi-t <nahomketema553@gmail.com>
2026-02-11 20:58:02 +0300

    updated

--- server/server/Rout/authrout.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import verifay from "../midleware/auth.js"
-const rout=express.Router()
-import {login,verify} from "../controler/authcontrol.js" 
-rout.post("/login",login)
-rout.get("/verify",verifay,verify)
-export default rout
--- server/server/Rout/salaryrout.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import verify from "../midleware/auth.js";
-import { addsalary, getsalary } from "../controler/salarycontrol.js";
-const rout = express.Router();
-rout.post("/adds", verify, addsalary);
-rout.get("/get/:id", verify, getsalary);
-export default rout;
